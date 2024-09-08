0d0c384cd235c25f2020f7eaa3d35a461a1cde07
Yean <kjan0929@naver.com>
2024-09-08 18:01:43 +0900

    style: 네비게이션 tab bar 구현

--- src/components/common/TabNavigator.tsx
+++ src/components/common/TabNavigator.tsx
@@ -1,13 +1,83 @@
-import styles from "../../styles/TabNavigator.module.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
+import styles from "../../styles/TabNavigator.module.css";
+import homeUnactiveIcon from "../../assets/nav/home-btn-unactive.svg";
+import homeActiveIcon from "../../assets/nav/home-btn-active.svg";
+import mapUnactiveIcon from "../../assets/nav/map-btn-unactive.svg";
+import mapActiveIcon from "../../assets/nav/map-btn-active.svg";
+import performanceUnactiveIcon from "../../assets/nav/performance-btn-unactive.svg";
+import performanceActiveIcon from "../../assets/nav/performance-btn-active.svg";
+import barUnactiveIcon from "../../assets/nav/bar-btn-unactive.svg";
+import barActiveIcon from "../../assets/nav/bar-btn-active.svg";
 
 export const TabNavigator = () => {
+  const [navStatus, setNavStatus] = useState<number>(1);
+
   return (
     <div className={styles.wrapper}>
-      <Link to={"/"}>홈</Link>
-      <Link to={"/map"}>지도</Link>
-      <Link to={"/performance"}>공연 정보</Link>
-      <Link to={"/bar"}>주점</Link>
+      <Link to={"/"} className={styles.link} onClick={() => setNavStatus(1)}>
+        <img
+          src={navStatus === 1 ? homeActiveIcon : homeUnactiveIcon}
+          alt="home-icon"
+        />
+        <strong
+          className={classNames(
+            styles.text,
+            navStatus === 1 ? styles.active : styles.unactive
+          )}
+        >
+          홈
+        </strong>
+      </Link>
+      <Link to={"/map"} className={styles.link} onClick={() => setNavStatus(2)}>
+        <img
+          src={navStatus === 2 ? mapActiveIcon : mapUnactiveIcon}
+          alt="map-icon"
+        />
+        <strong
+          className={classNames(
+            styles.text,
+            navStatus === 2 ? styles.active : styles.unactive
+          )}
+        >
+          지도
+        </strong>
+      </Link>
+      <Link
+        to={"/performance"}
+        className={styles.link}
+        onClick={() => setNavStatus(3)}
+      >
+        <img
+          src={
+            navStatus === 3 ? performanceActiveIcon : performanceUnactiveIcon
+          }
+          alt="performance-icon"
+        />
+        <strong
+          className={classNames(
+            styles.text,
+            navStatus === 3 ? styles.active : styles.unactive
+          )}
+        >
+          공연 정보
+        </strong>
+      </Link>
+      <Link to={"/bar"} className={styles.link} onClick={() => setNavStatus(4)}>
+        <img
+          src={navStatus === 4 ? barActiveIcon : barUnactiveIcon}
+          alt="bar-icon"
+        />
+        <strong
+          className={classNames(
+            styles.text,
+            navStatus === 4 ? styles.active : styles.unactive
+          )}
+        >
+          주점
+        </strong>
+      </Link>
     </div>
   );
 };
