e486666ffb551d687105a84cf386e513717e4f41
minjee2758 <minjee2758@naver.com>
2024-09-13 14:01:17 +0900

    style : 화면 비율 조정, 검색창 스타일 변경

--- src/pages/bar/bar.tsx
+++ src/pages/bar/bar.tsx
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import styles from '@styles/BarList/BarList.module.css'; 
 
-// 주점 데이터 명명
+// 주점 데이터 명명하기
 interface Store {
   name: string;
   representative: string;
   description: string;
 }
 
-// 주점 데이터 목록
+// 주점 데이터 목록 - 임시
 const stores: Store[] = [
   {
     name: '무간판',
     representative: '언론정보대학 학생회',
-    description: '답전, 유부우동&닭고치, 통영 산지직송',
+    description: '닭전, 유부우동&닭고치, 통영 산지직송',
   },
   {
     name: '교통은 쥐뿔도 있지만',
@@ -40,43 +40,66 @@ const stores: Store[] = [
     name: '(주)우아한재화들 배달의 민족',
     representative: '재료화학공학과 학생회',
     description: '마장동에서 갓 잡아온 육회, 을지로',
+  },
+  {
+    name: '(주)우아한재화들 배달의 민족',
+    representative: '재료화학공학과 학생회',
+    description: '마장동에서 갓 잡아온 육회, 을지로',
+  },
+  {
+    name: '(주)우아한재화들 배달의 민족',
+    representative: '재료화학공학과 학생회',
+    description: '마장동에서 갓 잡아온 육회, 을지로',
   }
 ];
+
 
 export const BarPage = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate(); 
 
-  const filteredStores = stores.filter(store =>
-    store.name.toLowerCase().includes(searchTerm.toLowerCase())
+  // 주점 검색 필터
+  const filteringStores = stores.filter(store =>
+    store.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    store.representative.toLowerCase().includes(searchTerm.toLowerCase()) || // 대표자 검색 가능
+    store.description.toLowerCase().includes(searchTerm.toLowerCase()) // 설명 부분 검색 가능
   );
+
 
   const handleStoreClick = (BarDetail: string) => {
     navigate(`/bar-detail/${BarDetail}`); 
   };
 
+
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>주점</h3>
-      <input
-        type="text"
-        placeholder="부스명, 학과, 메뉴를 검색해보세요"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-        className={styles.input}
-      />
-      <div>
-        {filteredStores.map((store, index) => (
-          <div 
-            key={index} 
-            className={styles.store} 
-            onClick={() => handleStoreClick(store.name)} 
-          >
-            <h2>{store.name}</h2>
-            <p>{store.representative}</p>
-            <p>{store.description}</p>
-          </div>
-        ))}
+      <div className={styles['search-container']}>
+        <div className={styles.title}>주점</div>
+        <input
+          type="text"
+          placeholder="부스명, 학과, 메뉴를 검색해보세요"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          className={`${styles.input} ${searchTerm ? styles.active : ''}`}
+        />
+      </div>
+      <div className={styles['store-list']}>
+        {filteringStores.length > 0 ? ( 
+          filteringStores.map((store, index) => (
+            <div 
+              key={index} 
+              className={styles.store} 
+              onClick={() => handleStoreClick(store.name)} 
+            >
+              <h2>{store.name}</h2>
+              <p>{store.representative}</p>
+              <p>{store.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>주점을 찾을 수 없습니다 T.T</p> //하냥이 일러스트 제작 후 추가 예정
+        )}
       </div>
     </div>
   );
