715abbd726bbb75757784afde1990cf7b84d89e4
minjee2758 <minjee2758@naver.com>
2024-09-14 21:35:10 +0900

    feat : 스크롤, 주점 이미지 추가
    자잘한 css 수정

--- src/pages/bar/bar.tsx
+++ src/pages/bar/bar.tsx
@@ -7,6 +7,7 @@ interface Store {
   name: string;
   representative: string;
   description: string;
+  imageUrl: string;
 }
 
 // 주점 데이터 목록 - 임시
@@ -15,41 +16,49 @@ const stores: Store[] = [
     name: '무간판',
     representative: '언론정보대학 학생회',
     description: '닭전, 유부우동&닭고치, 통영 산지직송',
+    imageUrl:'src/assets/bar/주점_무간판.png',
   },
   {
     name: '교통은 쥐뿔도 있지만',
     representative: '교통물류공학과 학생회',
     description: '불담쌈, 제육볶음, 오리훈제',
+    imageUrl:'',
   },
   {
     name: '토목갱~스껄',
     representative: '건설환경공학과 학생회',
     description: '김치전, 마늘보쌈, 삼겹버섯구이',
+    imageUrl:'',
   },
   {
     name: '산-자카야',
     representative: '산업경영공학과 학생회',
     description: '볶음우동, 대패삼겹속주먹밥',
+    imageUrl:'',
   },
   {
     name: '연어분! 참치마새우',
     representative: '해양융합공학과 학생회',
     description: '연어회, 참치회, 새우라면',
+    imageUrl:'',
   },
   {
     name: '(주)우아한재화들 배달의 민족',
     representative: '재료화학공학과 학생회',
     description: '마장동에서 갓 잡아온 육회, 을지로',
+    imageUrl:'',
   },
   {
     name: '(주)우아한재화들 배달의 민족',
     representative: '재료화학공학과 학생회',
     description: '마장동에서 갓 잡아온 육회, 을지로',
+    imageUrl:'',
   },
   {
     name: '(주)우아한재화들 배달의 민족',
     representative: '재료화학공학과 학생회',
     description: '마장동에서 갓 잡아온 육회, 을지로',
+    imageUrl:'',
   }
 ];
 
@@ -92,15 +101,19 @@ export const BarPage = () => {
               className={styles.store} 
               onClick={() => handleStoreClick(store.name)} 
             >
-              <div className={styles.storeName}>{store.name}</div>
-              <div className={styles.storeRepresentative}>{store.representative}</div>
-              <div className={styles.descriptions}>
-                <div className={styles.representMenu}>
-                  대표
+              <img src={store.imageUrl} alt={`${store.name} 이미지`} className={styles.storeImage} />
+              <div className={styles.storeDetails}>
+                <div className={styles.storeName}>{store.name}</div>
+                <div className={styles.storeRepresentative}>{store.representative}</div>
+                <div className={styles.descriptions}>
+                  <div className={styles.representMenu}>
+                    대표
+                  </div>
+                  {store.description}
                 </div>
-                {store.description}
               </div>
             </div>
+            
           ))
         ) : (
           //검색 키워드가 없을때
