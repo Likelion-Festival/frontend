a79a6c65a91501d69f707a4b2f85034f5da72e23
minjee2758 <minjee2758@naver.com>
2024-09-15 16:29:44 +0900

    style : 상세페이지 기본 디자인 설정

--- src/pages/bar/bar-detail.tsx
+++ src/pages/bar/bar-detail.tsx
@@ -22,20 +22,30 @@ export const BarDetail = () => {
     return (
         <div className={styles.container}>
             <div className={styles['barDetail-image']}>
-                <img src={store.imageUrl} alt={`${store.name} 이미지`} className={styles.barMainImage} />
+                <img src={'/src/assets/bar/주점1.png'} alt={`${store.name} 이미지`} className={styles.barMainImage} />
                 <img src={'/src/assets/bar/goBack.png'} alt={`뒤로 가기`} className={styles.goBack} onClick={handleGoBack} />
                 
                 <div className={styles.barDetail}>
-                    <h1>{store.name}</h1>
-                    <p>{store.description}</p>
-                    <h2>메뉴</h2>
-                    <ul>
-                        {store.menu.map((item: MenuItem, index: number) => (
-                            <li key={index}>
-                                {item.name} - {item.price}원
-                            </li>
-                        ))}
-                    </ul>
+                    <div className={styles['barDetail-title']}>
+                        <h1>{store.name}</h1>
+                        <p>{store.representative}</p>
+                        <button className={styles.locationButton}>
+                            지도로 위치 확인
+                        </button>
+                    </div>
+
+                    <div className={styles['barDetail-menu']}>
+                        <h3>메뉴</h3>
+                        <ul>
+                            {store.menu.map((item: MenuItem, index: number) => (
+                                <li key={index}>
+                                    {item.name}<br/>
+                                    {item.price}원
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         </div>
