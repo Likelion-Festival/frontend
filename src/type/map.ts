103f5b909af5185f1db8f5955878f41ed88b0c8d
Yean <kjan0929@naver.com>
2024-09-22 18:05:42 +0900

    feat: 지도 필터 컴포넌트 분리

--- src/type/map.ts
+++ src/type/map.ts
@@ -1,8 +1,7 @@
-export interface PlacesSearchProps {
-  id: string; // 장소 ID
-  place_name: string; // 장소 이름
-  category_name: string; // 카테고리 이름
-  x: number; // 경도
-  y: number; // 위도
-  // 필요한 다른 속성들 추가
+export interface MarkersType {
+  eventMarkers: kakao.maps.Marker[];
+  barMarkers: kakao.maps.Marker[];
+  foodCourtMarkers: kakao.maps.Marker[];
+  medicalMarkers: kakao.maps.Marker[];
+  smokingMarkers: kakao.maps.Marker[];
 }
