55e93b87f9f80c148839fe4b3d0d70d225120ca6
HiImSUN <kshk08@hanyang.ac.kr>
2024-09-24 00:17:12 +0900

    feat: NoticeType 제작

--- src/pages/notice/noticeList.tsx
+++ src/pages/notice/noticeList.tsx
@@ -1,11 +1,14 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import goBackImg from "@assets/notice/goBackButton.svg";
 import styles from "@styles/notice/noticeList.module.css";
 export const NoticeListPage = () => {
+  const navigate = useNavigate();
+  const handleGoBack = () => navigate(-1);
   return (
     <div className={styles.wrapper}>
+      <div className={styles.topMargin} />
       <div className={styles.header}>
-        <button className={styles.goBack}>
+        <button className={styles.goBack} onClick={handleGoBack}>
           <img src={goBackImg} alt="goBack" />
         </button>
         <div className={styles.entireTitle}>공지사항</div>
