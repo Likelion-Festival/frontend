07f437a275b300188491ef8016234179d4468c95
Yean <kjan0929@naver.com>
2024-09-23 22:26:16 +0900

    feat: 호수공원 이벤트 바텀 시트 예외처리

--- src/components/map/BottomSheet.tsx
+++ src/components/map/BottomSheet.tsx
@@ -5,17 +5,26 @@ import { BottomSheetHeader } from "./BottomSheetHeader";
 import { Content } from "./Content";
 import { useBottomSheet } from "@hooks/useBottomSheet";
 import { MapContext } from "./Map";
-import { markerInfo } from "@constant/marker";
+import { lakeParkInfo, markerInfo } from "@constant/marker";
 
 import { useContext } from "react";
 import { MarkerInfoType } from "@type/map";
 
 export const Bottomsheet = () => {
   const { sheet, content } = useBottomSheet();
-  const value = useContext(MapContext);
+  const value = useContext(MapContext); // 사용자가 클릭한 마커 위치 정보
 
   const markerList: MarkerInfoType[] = [];
 
+  // 호수공원 이벤트 예외처리
+  if (
+    lakeParkInfo[0].position.getLat() === value?.getLat() &&
+    lakeParkInfo[0].position.getLng() === value?.getLng()
+  ) {
+    lakeParkInfo.map((v) => markerList.push(v));
+  }
+
+  // 사용자 클릭 위치 정보와 일치하는 것 찾아내기
   const info = markerInfo.find((marker) => {
     const newLatLng = marker.position;
     return (
--- src/constant/marker.ts
+++ src/constant/marker.ts
@@ -1,6 +1,6 @@
 export const markerInfo = [
   {
-    id: 1,
+    id: 5,
     name: "한양보건센터",
     category: "의무실",
     time: `09:00~17:00
@@ -10,7 +10,7 @@ export const markerInfo = [
     imagePath: "/marker-img/medical.png",
   },
   {
-    id: 2,
+    id: 6,
     name: "플리마켓 존",
     category: "이벤트",
     time: `10:30~16:30`,
@@ -19,12 +19,51 @@ export const markerInfo = [
     imagePath: "/marker-img/flea-market.png",
   },
   {
-    id: 3,
+    id: 7,
     name: "프로모션 존",
     category: "이벤트",
     time: `10:30~16:30`,
     location: "학생복지관 앞",
     position: new kakao.maps.LatLng(37.296951, 126.835818),
     imagePath: "/marker-img/flea-market.png",
+  },
+];
+
+export const lakeParkInfo = [
+  {
+    id: 1,
+    name: "호공질주",
+    category: "이벤트",
+    time: `10월 2일 11:00~14:00`,
+    location: "호수공원",
+    position: new kakao.maps.LatLng(37.297633, 126.833848),
+    imagePath: "/marker-img/cross-the-lake.png",
+  },
+  {
+    id: 2,
+    name: "판타지아",
+    category: "이벤트",
+    time: `10월 2일 21:00~23:30`,
+    location: "호수공원",
+    position: new kakao.maps.LatLng(37.297633, 126.833848),
+    imagePath: "/marker-img/fantasy.png",
+  },
+  {
+    id: 3,
+    name: "에리카지노",
+    category: "이벤트",
+    time: `10월 1~2일 11:00~16:30`,
+    location: "호수공원",
+    position: new kakao.maps.LatLng(37.297633, 126.833848),
+    imagePath: "/marker-img/ericasino.png",
+  },
+  {
+    id: 4,
+    name: "이지공작소",
+    category: "이벤트",
+    time: `10월 1~2일 11:00~16:30`,
+    location: "호수공원",
+    position: new kakao.maps.LatLng(37.297633, 126.833848),
+    imagePath: "/marker-img/easy-workshop.png",
   },
 ];
