6e02cc42bcde8435cb4257fb9cd3e2f8636e1306
Yean <kjan0929@naver.com>
2024-09-24 00:30:59 +0900

    feat: 카테고리 및 마커 이벤트에 따른 바텀시트 항목 조건부 렌더링

--- src/components/map/Content.tsx
+++ src/components/map/Content.tsx
@@ -1,15 +1,38 @@
+import { lakeParkInfo, markerInfo } from "@constant/marker";
+import { useMapContext } from "@context/MapContext";
 import styles from "@styles/map/Content.module.css";
 import { MarkerInfoType } from "@type/map";
+import { useEffect, useState } from "react";
 
 interface ContentProps {
   markerInfoList?: MarkerInfoType[];
 }
 
 export const Content = ({ markerInfoList }: ContentProps) => {
+  const { currCategory } = useMapContext();
+  // 현재 선택된 카테고리에 해당되는 리스트 필터링
+  const categoryList = [...markerInfo, ...lakeParkInfo].filter(
+    (marker) => marker.category === currCategory
+  );
+
+  const [renderList, setRenderList] = useState<MarkerInfoType[]>(categoryList);
+
+  // 카테고리 리스트 렌더링을 기본으로
+  useEffect(() => {
+    setRenderList(categoryList);
+  }, [currCategory]);
+
+  // 개별 마커 클릭시에만 항목 렌더링
+  useEffect(() => {
+    if (markerInfoList && markerInfoList.length > 0) {
+      setRenderList(markerInfoList);
+    }
+  }, [markerInfoList]);
+
   return (
     <div className={styles.wrapper}>
       <ul className={styles.marker_list}>
-        {markerInfoList?.map((markerInfo) => {
+        {renderList.map((markerInfo) => {
           return (
             <li className={styles.content} key={markerInfo.id}>
               <div className={styles.content_info}>
